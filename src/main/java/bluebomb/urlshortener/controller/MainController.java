3ab657740a4f1d8673dbddc5c48970445517645a
AbelChT <abelcht@outlook.es>
2018-11-12 21:12:58 +0100

    QR impemented

--- src/main/java/bluebomb/urlshortener/controller/MainController.java
+++ src/main/java/bluebomb/urlshortener/controller/MainController.java
@@ -4,6 +4,8 @@ import bluebomb.urlshortener.errors.NotFoundException;
 import bluebomb.urlshortener.model.Browser;
 import bluebomb.urlshortener.model.QR;
 import bluebomb.urlshortener.model.URL;
+import bluebomb.urlshortener.qr.QRCodeGenerator;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,21 +17,21 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class MainController {
-    @RequestMapping(value = "/short", method= POST)
+    @RequestMapping(value = "/short", method = POST)
     public URL getShortURI(@RequestParam(value = "headURL") String headURL, @RequestParam(value = "interstitialURL", required = false) String interstitialURL) {
         return new URL(headURL);
     }
 
-    @RequestMapping(value = "/qr/{sequence}")
-    public QR getQR(@PathVariable(value = "sequence") String sequence){
-        if(sequence.equals("error")){
+    @RequestMapping(value = "/qr/{sequence}", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getQR(@PathVariable(value = "sequence") String sequence) {
+        if (sequence.equals("error")) {
             throw new NotFoundException();
         }
-        return new QR(sequence);
+        return QRCodeGenerator.generate(sequence);
     }
 
     @RequestMapping(value = "/browsers")
-    public ArrayList<Browser> getSupportedBrowsers(){
+    public ArrayList<Browser> getSupportedBrowsers() {
         ArrayList<Browser> toReturn = new ArrayList<>();
         toReturn.add(new Browser("IE"));
         toReturn.add(new Browser("EDGE"));
---
+++ src/main/java/bluebomb/urlshortener/errors/ServerErrorException.java
@@ -0,0 +1,8 @@
+package bluebomb.urlshortener.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "To show an example of a custom message")
+public class ServerErrorException extends RuntimeException {
+}
