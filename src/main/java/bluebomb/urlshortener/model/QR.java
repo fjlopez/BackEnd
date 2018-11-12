ef7904ad85c701f23815aa045b9788518b132fcc
AbelChT <abelcht@outlook.es>
2018-11-12 20:01:13 +0100

    Main controller mockup

---
+++ src/main/java/bluebomb/urlshortener/model/QR.java
@@ -0,0 +1,17 @@
+package bluebomb.urlshortener.model;
+
+public class QR {
+    private String image;
+
+    public QR(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
