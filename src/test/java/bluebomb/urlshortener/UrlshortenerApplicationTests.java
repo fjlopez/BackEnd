9aa4f46f53cc9459cfb017c2b7e5fef62b636e9f
Jorge Rambla Gonzalez <718911@unizar.es>
2018-11-12 23:54:07 +0100

    Begging with test

--- src/test/java/bluebomb/urlshortener/UrlshortenerApplicationTests.java
+++ src/test/java/bluebomb/urlshortener/UrlshortenerApplicationTests.java
@@ -1,16 +1,35 @@
 package bluebomb.urlshortener;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import bluebomb.urlshortener.controller.MainController;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UrlshortenerApplicationTests {
 
-    @Test
+    /*@Test
     public void contextLoads() {
+    }*/
+
+    @Test
+    public void testGetBrowsers() {
+        Object response = new MainController().getSupportedBrowsers();
+        assertNotNull(response);
+        assertTrue(response instanceof ArrayList);
+    }
+
+    @Test
+    public void testGetQr() {
+
     }
 
 }
