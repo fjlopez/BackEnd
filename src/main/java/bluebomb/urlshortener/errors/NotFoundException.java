d1021811fe210e0a25393eace803f1fe08aab761
Jorge Rambla Gonzalez <718911@unizar.es>
2018-11-12 23:52:47 +0100

    Serialized Exceptions

--- src/main/java/bluebomb/urlshortener/errors/NotFoundException.java
+++ src/main/java/bluebomb/urlshortener/errors/NotFoundException.java
@@ -4,4 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.FORBIDDEN, reason="To show an example of a custom message")
-public class NotFoundException extends RuntimeException { }
+public class NotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+}
--- src/main/java/bluebomb/urlshortener/errors/ServerErrorException.java
+++ src/main/java/bluebomb/urlshortener/errors/ServerErrorException.java
@@ -5,4 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "To show an example of a custom message")
 public class ServerErrorException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
 }
