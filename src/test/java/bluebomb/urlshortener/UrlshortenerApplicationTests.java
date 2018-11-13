9ced64ac76500ce185f07a23d1dff12e616b90ba
Jorge Rambla Gonzalez <718911@unizar.es>
2018-11-13 09:47:53 +0100

    Redirect controller test finish

--- src/test/java/bluebomb/urlshortener/UrlshortenerApplicationTests.java
+++ src/test/java/bluebomb/urlshortener/UrlshortenerApplicationTests.java
@@ -8,10 +8,10 @@ import java.util.ArrayList;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import bluebomb.urlshortener.controller.MainController;
+import bluebomb.urlshortener.controller.*;
+import bluebomb.urlshortener.model.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -38,7 +38,9 @@ public class UrlshortenerApplicationTests {
     //REDIRECT CONTROLLER
     @Test
     public void testRedirect() {
-        //TODO: implement Test
+        Object response = new RedirectController().redirect("BoMb9", "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36");
+        assertNotNull(response);
+        assertTrue(response instanceof RedirectURL);
     }
 
     //STATS CONTROLLER
