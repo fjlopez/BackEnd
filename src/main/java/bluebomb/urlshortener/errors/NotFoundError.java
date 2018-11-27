4023b1ee82ef31c4425b6a94865ab8e4ede89cc9
Abel Chils <abelcht@outlook.es>
2018-11-27 20:01:29 +0100

    errors changed

--- src/main/java/bluebomb/urlshortener/errors/NotFoundError.java
+++ src/main/java/bluebomb/urlshortener/errors/NotFoundError.java
@@ -3,8 +3,6 @@ package bluebomb.urlshortener.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Not found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not found")
 public class NotFoundError extends RuntimeException {
-    private String message;
-    private static final long serialVersionUID = 1L;
 }
