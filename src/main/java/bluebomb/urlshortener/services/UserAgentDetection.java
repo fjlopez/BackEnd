c67421f1991e4f04c78ed2ee6b6c3b2d4327e07e
AbelChT <abelcht@outlook.es>
2018-12-01 12:27:34 +0100

    User agent implemented

--- src/main/java/bluebomb/urlshortener/services/UserAgentDetection.java
+++ src/main/java/bluebomb/urlshortener/services/UserAgentDetection.java
@@ -1,8 +1,17 @@
 package bluebomb.urlshortener.services;
 
+import eu.bitwalker.useragentutils.UserAgent;
+
+/**
+ * Detect OS and Browser from an user agent
+ */
 public class UserAgentDetection {
     private static UserAgentDetection ourInstance = new UserAgentDetection();
 
+    /**
+     * Get an instance of the class (Singleton pattern)
+     * @return the instance of the class
+     */
     public static UserAgentDetection getInstance() {
         return ourInstance;
     }
@@ -13,22 +22,22 @@ public class UserAgentDetection {
     /**
      * Detect user agent OS
      *
-     * @param userAgent
-     * @return
+     * @param userAgentString user agent obtained from the client
+     * @return OS relative to this user agent
      */
-    public String detectOS(String userAgent){
-       // TODO:
-        return "NONE";
+    public String detectOS(String userAgentString){
+        UserAgent userAgent = UserAgent.parseUserAgentString(userAgentString);
+        return userAgent.getOperatingSystem().getName();
     }
 
     /**
      * Detect user agent browser
      *
-     * @param userAgent
-     * @return
+     * @param userAgentString user agent obtained from the client
+     * @return OS relative to this user agent
      */
-    public String detectBrowser(String userAgent){
-        // TODO:
-        return "NONE";
+    public String detectBrowser(String userAgentString){
+        UserAgent userAgent = UserAgent.parseUserAgentString(userAgentString);
+        return userAgent.getBrowser().getName();
     }
 }
